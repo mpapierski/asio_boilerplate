57eb009c3f1a4384e01c5254f52c544106094cef
Michał Papierski <michal@papierski.net>
2015-10-11 21:05:46 +0200

    Added support for "--daemon" flag.
    Daemonizing is optional. This flag should be used when the service
    should be used as daemon in the background. By default the service
    will be run in foreground. In case you use some process management
    external to your service you dont need any stuff to do unless
    a package is deployed on your linux distribution and you probably
    want to use init.d script (which is not yet provided).

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/program_options.hpp>
+#if defined(HAVE_FORK)
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#endif
 #include "tcp_server.hpp"
 #include "config.h"
 
@@ -12,6 +17,9 @@ int main(int argc, char * argv[])
 	desc.add_options()
 		("help", "show help message")
 		("version", "show version")
+#if defined(HAVE_FORK)
+		("daemon", "daemonize")
+#endif
 		("port,p", po::value<unsigned short>(&port)->required(), "port")
 	;
 	po::variables_map vm;
@@ -44,6 +52,64 @@ int main(int argc, char * argv[])
 		return 1;
 	}
 	boost::asio::io_service io_service;
+#if defined(HAVE_FORK)
+	if (vm.count("daemon"))
+	{
+		io_service.notify_fork(boost::asio::io_service::fork_prepare);
+		
+		pid_t pid = ::fork();
+		if (pid > 0)
+		{
+			// Parent process, return and keep running
+			return 0;
+		}
+		else if (pid == -1)
+		{
+			// Report error. Might be syslog call aswell.
+			std::cerr << "fork #1 failed" << std::endl;
+			std::exit(1);
+		}
+
+		// Make the process a new session leader. This detaches it from the
+		// terminal.
+		::setsid();
+
+		// A process inherits its working directory from its parent. This could be
+		// on a mounted filesystem, which means that the running daemon would
+		// prevent this filesystem from being unmounted. Changing to the root
+		// directory avoids this problem.
+		::chdir("/");
+
+		// The file mode creation mask is also inherited from the parent process.
+		// We don't want to restrict the permissions on files created by the
+		// daemon, so the mask is cleared.
+		::umask(0);
+
+		// Service forks here
+		pid = ::fork();
+		if (pid > 0)
+		{
+			// Parent process, return and keep running
+			std::exit(0);
+		}
+		else if (pid == -1)
+		{
+			std::cerr << "fork #2 failed" << std::endl;
+			std::exit(1);
+		}
+
+		// Close the standard streams. This decouples the daemon from the terminal
+		// that started it.
+		::close(0);
+		::close(1);
+		::close(2);
+
+		// Inform the io_service that we have finished becoming a daemon. The
+		// io_service uses this opportunity to create any internal file descriptors
+		// that need to be private to the new process.
+		io_service.notify_fork(boost::asio::io_service::fork_parent);
+	}
+#endif
 	try
 	{
 		// Prepare server
